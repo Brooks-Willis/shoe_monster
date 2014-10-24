d75372feeb4b69cc215cc891928e8bae1c8459b6
rachelboy <rachel.boy@gmail.com>
2014-10-24 13:32:55 -0400

    moved things because i forgot what folders are supposed to be named

--- scripts/testing_find_red_ball.py
+++ scripts/testing_find_red_ball.py
@@ -27,6 +27,25 @@ def find_red_center(img, convert_hsv=True):
 
 
 if __name__ == "__main__":
+
+    cap = cv2.VideoCapture(0)
+
+
+    while(cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
     img = cv2.imread('redball.jpg',1)
 
     posX, posY = find_red_center(img)
