d2d480b9b4a7ebf9b2d60685e47d5a776120988f
rachelboy <rachel.boy@gmail.com>
2014-10-24 13:27:41 -0400

    funcitonalized finding a red thing

--- src/testing_find_red_ball.py
+++ src/testing_find_red_ball.py
@@ -2,37 +2,43 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-# Load an color image in grayscale
-img = cv2.imread('redball.jpg',1)
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def find_red_center(img, convert_hsv=True):
+    if convert_hsv:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-bottom_red = np.array([0,50,50])
-lower_red = np.array([2,255,255])
-upper_red = np.array([160,50,50])
-top_red = np.array([179,255,255])
+    bottom_red = np.array([0,50,50])
+    lower_red = np.array([2,255,255])
+    upper_red = np.array([160,50,50])
+    top_red = np.array([179,255,255])
 
-mask1 = cv2.inRange(hsv, bottom_red, lower_red)
-mask2 = cv2.inRange(hsv, upper_red, top_red)
-mask = cv2.bitwise_or(mask1, mask2)
+    mask1 = cv2.inRange(img, bottom_red, lower_red)
+    mask2 = cv2.inRange(img, upper_red, top_red)
+    mask = cv2.bitwise_or(mask1, mask2)
 
-M = cv2.moments(mask)
+    #consider doing erode/dilate cycle to clean up object
 
-posX = int(M['m10']/M['m00'])
-posY = int(M['m01']/M['m00'])
+    M = cv2.moments(mask)
 
-cv2.circle(img,(posX,posY),2,(255,0,0))
+    posX = int(M['m10']/M['m00'])
+    posY = int(M['m01']/M['m00'])
 
-res = cv2.bitwise_and(img,img, mask= mask)
+    return posX, posY
 
-cv2.imshow('mask',mask)
-cv2.imshow('image',img)
-cv2.imshow('res',res)
-k = cv2.waitKey(0) & 0xFF
-if k == 27:         # wait for ESC key to exit
-    cv2.destroyAllWindows()
-elif k == ord('s'): # wait for 's' key to save and exit
-    cv2.imwrite('messigray.png',img)
-    cv2.destroyAllWindows()
 
+if __name__ == "__main__":
+    img = cv2.imread('redball.jpg',1)
+
+    posX, posY = find_red_center(img)
+
+    cv2.circle(img,(posX,posY),2,(255,0,0),5)
+
+    cv2.imshow('image',img)
+
+    k = cv2.waitKey(0) & 0xFF
+    if k == 27:         # wait for ESC key to exit
+        cv2.destroyAllWindows()
+    elif k == ord('s'): # wait for 's' key to save and exit
+        cv2.imwrite('messigray.png',img)
+        cv2.destroyAllWindows()
 
