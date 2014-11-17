796fd865a39735396ed43a9303b9b458485a7de9
rachelboy <rachel.boy@gmail.com>
2014-11-16 23:03:50 -0500

    commented testing_track_shoe

--- scripts/testing_track_shoe.py
+++ scripts/testing_track_shoe.py
@@ -22,7 +22,11 @@ class ObjectTracking:
         self.camera_listener = rospy.Subscriber("camera/image_raw",Image, self.track_object)
         self.target_pub = rospy.Publisher("target", Target)
         self.bridge = CvBridge()
+
+        # List of identifiers to consider when tracking the object
+        # identifiers should implement AbstractTracker from track_shoe
         self.identifiers = [HistTracker(),KeypointTracker()]
+
         self.i = 0
         self.state = self.SELECTING_QUERY_IMG
         self.query_roi=None
@@ -33,9 +37,9 @@ class ObjectTracking:
         cv2.namedWindow("Chase the Shoe")
         cv2.setMouseCallback("Chase the Shoe",self.mouse_event)
 
-        # print "initiated"
 
     def show_img(self):
+        '''vizualise the image with the tracking data'''
         if self.state == self.SELECTING_QUERY_IMG:
             if self.query_roi != None: #Runs after selecting inital roi
                 # add the query image to the side
@@ -47,7 +51,7 @@ class ObjectTracking:
                 combined_img[0:(self.query_roi[3]-self.query_roi[1]),self.current_frame.shape[1]:,:] = (
                         self.query_img[self.query_roi[1]:self.query_roi[3],
                                           self.query_roi[0]:self.query_roi[2],:])
-                # print self.current_center
+                # draw where we think the center is
                 cv2.circle(combined_img,self.current_center,2,(255,0,0),10)
 
                 cv2.imshow("Chase the Shoe",combined_img)
@@ -63,9 +67,9 @@ class ObjectTracking:
         return float(tot_v)/tot_p
 
     def track_object(self,msg):
+        '''do the object tracking!'''
 
         # Bridge for color image. This page was very useful for deteermaning image types: http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
-        # print "hello"
         cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         image = np.asanyarray(cv_image)
         self.current_frame = image
@@ -73,28 +77,31 @@ class ObjectTracking:
         objs = []
         probs = []
         for idr in self.identifiers:
+            # get the hypothesized center from every tracker
             prob, obj_center = idr.track(image.copy(),viz=True)
-            if prob > .7:
+            if prob > .7:  #only consider probs over 70%
                 probs.append(prob)
                 objs.append(obj_center)
         if len(objs)>0:
+            # take the weighted average center
+            # alternative: take the most probable center
+            # (that's probably better, honestly. Why didn't we do that?)
             xs,ys = zip(*objs)
-            #TODO - Replace this with a weighted average
             out = Target(x = int(self.weighted_average(probs,xs)),
                          y = int(self.weighted_average(probs,ys)),
                          x_img_size=self.query_img.shape[0],
                          y_img_size=self.query_img.shape[1])
         else:
             out = Target(x=-1,y=-1,x_img_size=-1,y_img_size=-1)
-        # print prob
+
         self.current_center = out.x,out.y
-        # print self.current_center
-        # print self.current_frame.shape
         self.target_pub.publish(out)
         self.show_img()
         
 
     def mouse_event(self,event,x,y,flag,im):
+        '''select the region of interest! Yeah, we took this pretty much directly
+        from your stuff, Paul'''
         if event == cv2.EVENT_FLAG_LBUTTON:
             if self.state == self.SELECTING_QUERY_IMG:
                 self.query_img_visualize = self.current_frame.copy()
@@ -117,23 +124,3 @@ if __name__ == "__main__":
     rospy.init_node('track_object', anonymous=True)
     object_tracker = ObjectTracking()
     rospy.spin()
-    # cap = cv2.VideoCapture(-1)
-    # tracker = track.RedTracker()
-
-    # while(True):
-    #     # Capture frame-by-frame
-    #     ret, frame = cap.read()
-
-    #     posX, posY = tracker.find_red_center(frame)
-
-    #     # Our operations on the frame come here
-    #     cv2.circle(frame,(posX,posY),2,(255,0,0),5)
-
-    #     cv2.imshow('image',frame)
-
-    #     if cv2.waitKey(1) & 0xFF == ord('q'):
-    #         break
-
-    # # When everything done, release the capture
-    # cap.release()
-    # cv2.destroyAllWindows()
